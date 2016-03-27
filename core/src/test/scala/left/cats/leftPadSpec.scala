4a11ebcaeaf4fb49d87641e6623fc399d9f53efd
Rob Norris <rob_norris@mac.com>
2016-03-27 13:27:06 -0700

    remove constraint

--- core/src/test/scala/left/cats/leftPadSpec.scala
+++ core/src/test/scala/left/cats/leftPadSpec.scala
@@ -6,14 +6,12 @@ object LeftPadSpec extends Properties("LeftPad") {
   import Prop._
   import LeftPad._
 
-  property("no-op") = forAll { (w: Char, s: String) =>
+  property("zero") = forAll { (w: Char, s: String) =>
     LeftPad[String, Char].leftPad(s)(s.length, w) == s
   }
 
   property("shorter") = forAll { (w: Char, s: String) =>
-    s.length > 0 ==> {
-      LeftPad[String, Char].leftPad(s)(s.length - 1, w) == s
-    }
+    LeftPad[String, Char].leftPad(s)(s.length - 3, w) == s
   }
 
   property("longer") = forAll { (w: Char, s: String) =>
