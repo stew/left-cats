78a1e7469616b1d6bac8b094718d343aec4e6e29
Stew O'Connor <stew.oconnor@verizon.com>
2016-03-27 18:53:57 -0700

    merge my tests with rob's

--- core/src/test/scala/LeftPadSpec.scala
+++ core/src/test/scala/LeftPadSpec.scala
@@ -18,5 +18,17 @@ object LeftPadSpec extends Properties("left-pad") {
   property("alternative foldable") = forAll{(a: Vector[Char], c: Char, n: Int) =>
       LeftPad[Vector[Char],Char].leftPad(a)(n,c).foldMap(_.toString) == LeftPad[String,Char].leftPad(a.mkString(""))(n,c)
   }
+
+  property("zero") = forAll { (w: Char, s: String) =>
+    LeftPad[String, Char].leftPad(s)(s.length, w) == s
+  }
+
+  property("shorter") = forAll { (w: Char, s: String) =>
+    LeftPad[String, Char].leftPad(s)(s.length - 3, w) == s
+  }
+
+  property("longer") = forAll { (w: Char, s: String) =>
+    LeftPad[String, Char].leftPad(s)(s.length + 3, w) == w.toString * 3 + s
+  }
 }
 
