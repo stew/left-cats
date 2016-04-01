d6c83c6eaa974053329036526ff649c5d419ca16
Marc <marc.siegel@timgroup.com>
2016-04-01 09:00:53 -0400

    Whitespace consistency

--- core/src/test/scala/LeftPadSpec.scala
+++ core/src/test/scala/LeftPadSpec.scala
@@ -9,14 +9,14 @@ object LeftPadSpec extends Properties("left-pad") {
   implicit val arbInt: Arbitrary[Int] = Arbitrary(Gen.choose(-100, 100))
 
   property("string") = forAll { (a: String, c: Char, n: Int) =>
-    LeftPad[String,Char].leftPad(a)(n,c) == (if(n < a.length) a else (a.reverse ++ (c.toString * n)).take(n).reverse)
+    LeftPad[String, Char].leftPad(a)(n, c) == (if (n < a.length) a else (a.reverse ++ (c.toString * n)).take(n).reverse)
   }
 
   import cats._
   import cats.implicits._
 
   property("alternative foldable") = forAll { (a: Vector[Char], c: Char, n: Int) =>
-    LeftPad[Vector[Char],Char].leftPad(a)(n,c).foldMap(_.toString) == LeftPad[String,Char].leftPad(a.mkString(""))(n,c)
+    LeftPad[Vector[Char], Char].leftPad(a)(n, c).foldMap(_.toString) == LeftPad[String,Char].leftPad(a.mkString(""))(n, c)
   }
 
   property("zero") = forAll { (w: Char, s: String) =>
